e45ed7d86379bd99e857ece4b3411c83b93d5547
Charlie Bowen <charlie.bowenn@outlook.com>
2021-10-28 14:46:16 +0100

    Refine Sign Up Form
    - Add Sign Up form unit tests
    - Add password validation to sign up form

--- microblogs/forms.py
+++ microblogs/forms.py
@@ -1,10 +1,39 @@
+from django.core.validators import RegexValidator
 from django import forms
 from .models import User
+
 class SignUpForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'bio']
         widgets = { 'bio': forms.Textarea() }
 
-    new_password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+        regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+        message = 'Password must contain an uppercase, lowercase and number.'
+        )
+            # RegexValidator(
+            #     regex=r'[A-Z]',
+            #     message='Password must contain an uppercase character.'
+            # ),
+            # RegexValidator(
+            #     regex=r'[a-z]',
+            #     message='Password must contain a lowercase character.'
+            # ),
+            # RegexValidator(
+            #     regex=r'[0-9]',
+            #     message='Password must contain a number.'
+            # )
+        ]
+    )
     password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Password and password confirmation do not match.')
